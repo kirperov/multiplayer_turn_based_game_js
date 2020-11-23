6205eb923dd151cec9a8adda6fed5e14a8a5f83c
kirperov <kirill.petrov.centrale@gmail.com>
2020-11-23 16:41:37 +0100

    generate random obstacles on map

--- public/js/generateMap.js
+++ public/js/generateMap.js
@@ -1,34 +1,44 @@
 
+
 let elements = {
-    obstacle:"ob",
-    playerOne:"p1",
-    playerTwo:"p2",
-    weapon:"w"
+    obstacles: ["ob1", "ob2", "ob3", "ob4"],
+    players: ["p1", "p2"],
+    weapons: ["w1", "w2", "w3", "w4"],
+    map:[
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]
+    ]
 }
 
 export default class GenerateMap {
+    constructor(elements) {
+        this.elements = elements;
+    }
     generateRandomNumber(num) {
         return Math.floor((Math.random() * num));
     }
-    arrayMap(elements) {
-        let tableau = [
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0]
-        ];  
-        for(let i = 0; i < tableau.length; i++) {
-            let numberRand = this.generateRandomNumber(tableau[i].length);
-            tableau[i].splice(numberRand,1,elements.obstacle);
-            console.log(tableau[i])
+
+    generateMap() {
+        let generatedMap=[];
+        for(let i = 0; i < this.elements.map.length; i++) {
+            let numberRand = this.generateRandomNumber(this.elements.map[i].length);
+            for(let n = 0; n<this.elements.obstacles.length;n++) {
+                let obstNbRand = this.generateRandomNumber(this.elements.obstacles[n].length);
+                this.elements.map[i].splice(numberRand,1,this.elements.obstacles[obstNbRand]);
+            }
+            generatedMap.push(this.elements.map[i]);
+            console.log(generatedMap[i]);
         }
     }
 }
-let generateMap=new GenerateMap();
-generateMap.arrayMap(elements);
+let generateMap=new GenerateMap(elements);
+generateMap.generateMap();
+ 
