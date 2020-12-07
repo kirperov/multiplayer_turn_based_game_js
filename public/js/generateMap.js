dfcb0fb2b8342964ec0c75d8d4cface5deb3c9b1
kirperov <kirill.petrov.centrale@gmail.com>
2020-12-07 11:43:43 +0100

    fix generate weapons correctly

--- public/js/generateMap.js
+++ public/js/generateMap.js
@@ -1,69 +1,79 @@
-export default class GenerateMap {
-    constructor(obstacles, players, weapons) {
+export default class Map {
+    constructor(x, y, obstacles, players, weapons) {
+        this.x = x;
+        this.y = y;
         this.obstacles = obstacles;
         this.players = players;
         this.weapons = weapons;
+        this.generatedMap = [];
     }
 
-    createMatrix(x,y) {
-        let map = [],
-            row = [],
-            boxes = 0;
-        //axe x
-        for(let i = 0; i < x; i++) {
-            row.push(boxes);
+    createMatrix() {
+        for(let i = 0; i < this.y; i++) {
+            //axe y
+            this.generatedMap[i] = [];
+            for(let n = 0; n < this.x; n++) {
+                //axe x
+                this.generatedMap[i][n] = 0;
+            }
         }
-        //axe y
-        for(let i = 0; i < y; i++) {
-            map.push(row);
-        }
-        return map;
     }
 
     generateRandomNumber(num) {
         return Math.floor((Math.random() * num));
     }
     
-    genrateObstacle(numberRand, matrix, generatedMap) {
-        for(let n = 0; n < numberRand; n++) {
-            let obstNbRand = this.generateRandomNumber(this.obstacles.length);
-            let caseNbRand = this.generateRandomNumber(matrix.length);
-            matrix[caseNbRand] = this.obstacles[obstNbRand];
+    genrateObstacles() {
+        for(let i = 0; i < this.y; i++) {
+            let numberRand = this.generateRandomNumber(this.y * 0.4);
+            for(let n = 0; n < numberRand; n++) {
+                let obstNbRand = this.generateRandomNumber(this.obstacles.length);
+                let caseNbRand = this.generateRandomNumber(this.y);
+                if(this.generatedMap[i][caseNbRand] == 0) {
+                    this.generatedMap[i][caseNbRand] = this.obstacles[obstNbRand];
+                }
+            }
         }
-        generatedMap.push(matrix);
     }
 
-    generatePlayers(generatedMap) {
+    generatePlayers() {
         let players = this.players;
         for(let i = 0; i < players.length; i++) {
-            let nbRand = this.generateRandomNumber(generatedMap.length);   
-            let nbRand2 = this.generateRandomNumber(generatedMap[i].length);   
-            generatedMap[nbRand].splice(nbRand2, 1, players[i]);
+            let positioned = false;
+            while(positioned == false) {
+                let nbRand = this.generateRandomNumber(this.generatedMap.length);   
+                let nbRand2 = this.generateRandomNumber(this.generatedMap[i].length);   
+                if(this.generatedMap[nbRand][nbRand2] == 0) {
+                    this.generatedMap[nbRand][nbRand2] = players[i];
+                    positioned = true;
+                }
+                console.log("x : " +  nbRand2 + "; y : " + nbRand + this.players[i]);
+            }
         }
     }
 
-    generateWeapons(generatedMap) {
+    generateWeapons() {
         let weapons = this.weapons;
         for(let i = 0; i < weapons.length; i++) {
-            let nbRand = this.generateRandomNumber(generatedMap.length);   
-            let nbRand2 = this.generateRandomNumber(generatedMap[i].length);   
-            generatedMap[nbRand].splice(nbRand2, 1, weapons[i]);
+            let positioned = false;
+            while(positioned == false) {
+                let nbRandY = this.generateRandomNumber(this.generatedMap.length);   
+                let nbRandX = this.generateRandomNumber(this.generatedMap[i].length);
+                if(this.generatedMap[nbRandY][nbRandX] == 0) {
+                    this.generatedMap[nbRandY][nbRandX] = weapons[i];
+                    positioned = true;
+                }
+            }
         }
     }
 
     generateMap() {
-        let matrix = this.createMatrix(10,10);
-        let generatedMap = [];
-        for(let i = 0; i < matrix.length; i++) {
-            matrix = this.createMatrix(10,10);
-            let numberRand = this.generateRandomNumber(matrix[i].length * 0.4);
-            this.genrateObstacle(numberRand, matrix[i], generatedMap);
-        }
-        this.generatePlayers(generatedMap);
-        this.generateWeapons(generatedMap);
-    
-        for(let i = 0; i <generatedMap.length; i++) {
-            console.log(generatedMap[i]);
+        this.createMatrix(this.x, this.y);
+        this.genrateObstacles();
+        this.generatePlayers();
+        this.generateWeapons();
+        for(let i = 0; i <this.generatedMap.length; i++) {
+            console.log(this.generatedMap[i]);
         }
     }
 }
@@ -71,7 +81,7 @@ export default class GenerateMap {
 let obstacles = ["ob1", "ob2", "ob3", "ob4"],
     players = ["p1", "p2"],
     weapons = ["w1", "w2", "w3", "w4"];
-let generateMap=new GenerateMap(obstacles, players, weapons);
+let generateMap=new Map(10, 10, obstacles, players, weapons);
 generateMap.generateMap();
  
  
